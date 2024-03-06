9321447faad9aad42147a17c6063d38d2949e54c
Deer-Spangle <deer@spangle.org.uk>
2024-03-06 23:23:03 +0000

    Switch from flask to quart for async

--- fa_rss/app.py
+++ fa_rss/app.py
@@ -1,27 +1,32 @@
 import json
+import logging
+import os
 import pathlib
+import sys
+from logging.handlers import TimedRotatingFileHandler
 
-from flask import Flask, render_template, abort, make_response
+from quart import Quart, render_template, abort, make_response
 
 from fa_rss.data_fetcher import DataFetcher
 from fa_rss.database.database import Database
 from fa_rss.faexport_client import FAExportClient
 
-app = Flask(__name__, template_folder=pathlib.Path(__file__).parent.parent / "templates")
+app = Quart(__name__, template_folder=str(pathlib.Path(__file__).parent.parent / "templates"))
 
 with open("config.json") as f:
     CONFIG = json.load(f)
 DB = Database(CONFIG["database"])
 API = FAExportClient(CONFIG["faexport"])
 FETCHER = DataFetcher(DB, API)
+# app.add_background_task(FETCHER.run_data_watcher)
 
 
-@app.route("/")
+@app.get("/")
 def home_page():
     return "Hey there. This project is a prototype. Quite an early one."
 
 
-@app.route('/user/<username>/<gallery>.rss')
+@app.get('/user/<username>/<gallery>.rss')
 async def gallery_feed(username, gallery):
     if gallery not in ["gallery", "scraps"]:
         abort(404)
@@ -29,15 +34,35 @@ async def gallery_feed(username, gallery):
     if user_data is None:
         await FETCHER.initialise_user_data(username)
     user_gallery = await DB.list_submissions_by_user_gallery(username, gallery)
-    rss_xml = render_template(
+    rss_xml = await render_template(
         "gallery_feed.rss.jinja2",
         username=username,
         gallery=gallery,
         submissions=user_gallery,
     )
-    response = make_response(rss_xml)
+    response = await make_response(rss_xml)
     response.headers['Content-Type'] = 'application/rss+xml'
     return response
+
+
+def setup_logging() -> None:
+    os.makedirs("logs", exist_ok=True)
+    formatter = logging.Formatter("{asctime}:{levelname}:{name}:{message}", style="{")
+
+    base_logger = logging.getLogger()
+    base_logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    base_logger.addHandler(console_handler)
+
+    # FA-RSS log, for diagnosing the service. Should not contain user information.
+    fa_logger = logging.getLogger("fa_rss")
+    file_handler = TimedRotatingFileHandler("logs/fa_rss.log", when="midnight")
+    file_handler.setFormatter(formatter)
+    fa_logger.addHandler(file_handler)
+
+
+setup_logging()
 
 
 if __name__ == '__main__':
