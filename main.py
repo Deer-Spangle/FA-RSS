98aaa8a5e675b5b965d8d9e4cb68d8f1e947075d
Deer-Spangle <deer@spangle.org.uk>
2024-03-06 23:23:26 +0000

    Make a main script which can run the fetcher or the server

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from fa_rss.app import app
+import asyncio
+import sys
+
+from fa_rss.app import app, FETCHER
+
 
 if __name__ == '__main__':
-    app.run()
+    cmd = sys.argv[1]
+    if cmd == "data_fetcher":
+        asyncio.get_event_loop().run_until_complete(FETCHER.run_data_watcher())
+    elif cmd == "server":
+        app.run()
+    else:
+        raise ValueError(f"Unrecognised command: {cmd}")
